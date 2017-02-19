1da710126ce3220e8ab98194d8acbd1beaa0c8b6
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-19 18:21:29 +0530

    creating and finding by id

--- uixml/src/main/java/com/codekul/uixml/MainActivity.java
+++ uixml/src/main/java/com/codekul/uixml/MainActivity.java
@@ -2,6 +2,9 @@ package com.codekul.uixml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtAnyName = (EditText) findViewById(R.id.edtAnyName);
+
+        Button btnOkay = (Button) findViewById(R.id.btnOkay);
     }
 }
