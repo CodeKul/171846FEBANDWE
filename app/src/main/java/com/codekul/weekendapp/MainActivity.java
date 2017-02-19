e0192d285f8f41419b6446b5d7002910d4393bee
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-19 17:54:54 +0530

    - creating ui through code - layout , views, context, activity, appcompact activity

--- app/src/main/java/com/codekul/weekendapp/MainActivity.java
+++ app/src/main/java/com/codekul/weekendapp/MainActivity.java
@@ -2,12 +2,35 @@ package com.codekul.weekendapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        MainActivity m = this;
+
+        LinearLayout.LayoutParams paramsRoot =
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(paramsRoot);
+
+        LinearLayout.LayoutParams paramsEdt = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        EditText edtUserName = new EditText(m /*activity is a context*/);
+        edtUserName.setLayoutParams(paramsEdt);
+        layout.addView(edtUserName);
+
+        LinearLayout.LayoutParams paramsBtn = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        Button btnOkay = new Button(this);
+        btnOkay.setText("Okay");
+        btnOkay.setLayoutParams(paramsBtn);
+        layout.addView(btnOkay);
+
+        setContentView(layout);
     }
 }
