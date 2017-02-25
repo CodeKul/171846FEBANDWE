de3c31739fe840a2d9cad15b450c6556132625b7
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-25 16:46:55 +0530

    event handling : creating simple inner class

--- uixml/src/main/java/com/codekul/uixml/MainActivity.java
+++ uixml/src/main/java/com/codekul/uixml/MainActivity.java
@@ -2,19 +2,33 @@ package com.codekul.uixml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edtAnyName = (EditText) findViewById(R.id.edtAnyName);
-
         Button btnOkay = (Button) findViewById(R.id.btnOkay);
+        btnOkay.setOnClickListener(new Click());
+    }
+
+    private class Click implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            EditText edtAnyName = (EditText) findViewById(R.id.edtAnyName);
+            String data = edtAnyName.getText().toString();
+            edtAnyName.setText(data.toUpperCase());
+
+            Log.i(TAG, data);
+        }
     }
 }
