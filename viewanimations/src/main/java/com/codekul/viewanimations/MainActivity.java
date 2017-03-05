c2f8e57315821fd5aab63814c1da6319f34558e7
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-05 11:58:45 +0530

    view animations : zoom, rotate and mixed animations

--- viewanimations/src/main/java/com/codekul/viewanimations/MainActivity.java
+++ viewanimations/src/main/java/com/codekul/viewanimations/MainActivity.java
@@ -2,6 +2,8 @@ package com.codekul.viewanimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnRotate).setOnClickListener(v -> animate(R.anim.rotate));
+        findViewById(R.id.btnZoom).setOnClickListener(v -> animate(R.anim.zoom));
+        findViewById(R.id.btnMixed).setOnClickListener(v -> animate(R.anim.mixed));
+    }
+
+    private void animate(int res) {
+        Animation animation = AnimationUtils.loadAnimation(this, res);
+        findViewById(R.id.imageView).startAnimation(animation);
     }
 }
