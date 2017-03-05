4997ddff9adaebe05e886e3a2a727be206201579
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-05 11:20:52 +0530

    bouncing ball : integration of rxjava

--- bouncingball/src/main/java/com/codekul/bouncingball/BallView.java
+++ bouncingball/src/main/java/com/codekul/bouncingball/BallView.java
@@ -1,10 +1,17 @@
 package com.codekul.bouncingball;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.widget.TextView;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by aniruddha on 5/3/17.
@@ -28,6 +35,14 @@ public class BallView extends TextView {
         ball = new Ball();
         paint = new Paint();
         paint.setColor(Color.RED);
+
+        ballMover()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext( integers -> invalidate())
+                .doOnError( e -> Log.i("@codekul", e.toString()))
+                .doOnComplete( () -> Log.i("@codekul", "Completed"))
+                .subscribe();
     }
 
     @Override
@@ -35,5 +50,21 @@ public class BallView extends TextView {
         super.onDraw(canvas);
 
         canvas.drawCircle(ball.x, ball.y, ball.rad, paint);
+    }
+
+    public Flowable<Integer[]> ballMover() {
+        return Flowable.create(sub -> {
+            while(true) {
+                if(ball.x < 0) ball.dx = 5;
+                if(ball.x >= getWidth()) ball.dx = -5;
+                if(ball.y < 0) ball.dy = 5;
+                if(ball.y >= getHeight()) ball.dy = -5;
+
+                ball.x += ball.dx;
+                ball.y += ball.dy;
+                Thread.sleep(10);
+                sub.onNext(new Integer[]{ball.x, ball.y});
+            }
+        }, BackpressureStrategy.BUFFER);
     }
 }
