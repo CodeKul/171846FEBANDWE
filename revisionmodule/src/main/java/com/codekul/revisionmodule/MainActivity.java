df709e4a2cf7cafe4bb0402fe345d91d66805844
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-19 18:08:19 +0530

    revision : creating layout via code

--- revisionmodule/src/main/java/com/codekul/revisionmodule/MainActivity.java
+++ revisionmodule/src/main/java/com/codekul/revisionmodule/MainActivity.java
@@ -1,7 +1,10 @@
 package com.codekul.revisionmodule;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +12,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        viCode();
+    }
+
+    private void viCode() {
+
+        MainActivity m = this;
+        Context c = m;
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(500,500));
+
+        Button btn = new Button(this);
+        btn.setLayoutParams(new LinearLayout.LayoutParams(500,150));
+        btn.setText("Android");
+        layout.addView(btn);
+
+        setContentView(layout);
     }
 }
