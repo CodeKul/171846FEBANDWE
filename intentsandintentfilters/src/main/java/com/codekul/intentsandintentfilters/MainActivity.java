a9a3e66a8d043682cd35b2ce77acdc05b1b99932
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-01 12:46:33 +0530

    intent, intent filters : implementing intent filters

--- intentsandintentfilters/src/main/java/com/codekul/intentsandintentfilters/MainActivity.java
+++ intentsandintentfilters/src/main/java/com/codekul/intentsandintentfilters/MainActivity.java
@@ -1,7 +1,10 @@
 package com.codekul.intentsandintentfilters;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void workWithIntents(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.codekul.action.COMMON");
+        intent.setData(Uri.parse("tel://q39uaoisfjf"));
+        startActivity(intent);
     }
 }
