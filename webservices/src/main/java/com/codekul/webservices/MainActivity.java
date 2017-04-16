65d59aec096591eac35354bdf6ec12e08e9131df
aniruddha <aniruddha.kudalkar@gmail.com>
2017-04-16 11:24:54 +0530

    web services : posting weather info to firebase

--- webservices/src/main/java/com/codekul/webservices/MainActivity.java
+++ webservices/src/main/java/com/codekul/webservices/MainActivity.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.codekul.webservices.domain.CityWeather;
 import com.codekul.webservices.domain.Main;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
         App app = (App) getApplication();
         app.q().add(
                 new StringRequest(
-                    "http://api.openweathermap.org/data/2.5/weather?q=" + city() + "?mode=json&units=metric&cnt=7&appid=7406c21bb1cb9f59d936a59c4e890279",
-                    this::onSuccess,
-                    this::onError
-        ));
+                        "http://api.openweathermap.org/data/2.5/weather?q=" + city() + "?mode=json&units=metric&cnt=7&appid=7406c21bb1cb9f59d936a59c4e890279",
+                        this::onSuccess,
+                        this::onError
+                ));
     }
 
     private void onError(VolleyError volleyError) {
@@ -41,11 +50,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onSuccess(String s) {
-        Log.i(TAG, "JSON - "+s);
-        CityWeather weather = ((App)getApplication()).gson().fromJson(s, CityWeather.class);
+        Log.i(TAG, "JSON - " + s);
+        CityWeather weather = ((App) getApplication()).gson().fromJson(s, CityWeather.class);
+
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("tmp", weather.getMain().getTemp());
+            obj.put("psr", weather.getMain().getPressure());
+            obj.put("minTmp", weather.getMain().getTempMin());
+            obj.put("maxTmp", weather.getMain().getTempMax());
+            obj.put("msl", weather.getMain().getSeaLevel());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ((App) getApplication()).q()
+                .add(
+                        new JsonObjectRequest(
+                        "https://digital-shelter-153912.firebaseio.com/weather.json",
+                        obj,
+                        this::onPostWeather,
+                        this::onError){
+                            @Override
+                            public Map<String, String> getHeaders() throws AuthFailureError {
+                                Map<String, String> map = new HashMap<>();
+
+                                return map;
+                            }
+                        }
+                );
 
         weather(weather.getMain());
         pd.dismiss();
+    }
+
+    private void onPostWeather(JSONObject obj) {
+
     }
 
     private String city() {
@@ -53,11 +93,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void weather(Main main) {
-        ((TextView)findViewById(R.id.textInfo)).setText("");
-        ((TextView)findViewById(R.id.textInfo)).append("\n Temp - "+main.getTemp());
-        ((TextView)findViewById(R.id.textInfo)).append("\n Pressure - "+main.getPressure());
-        ((TextView)findViewById(R.id.textInfo)).append("\n Min - "+main.getTempMin());
-        ((TextView)findViewById(R.id.textInfo)).append("\n Max - "+main.getTempMax());
-        ((TextView)findViewById(R.id.textInfo)).append("\n MSL - "+main.getSeaLevel());
+        ((TextView) findViewById(R.id.textInfo)).setText("");
+        ((TextView) findViewById(R.id.textInfo)).append("\n Temp - " + main.getTemp());
+        ((TextView) findViewById(R.id.textInfo)).append("\n Pressure - " + main.getPressure());
+        ((TextView) findViewById(R.id.textInfo)).append("\n Min - " + main.getTempMin());
+        ((TextView) findViewById(R.id.textInfo)).append("\n Max - " + main.getTempMax());
+        ((TextView) findViewById(R.id.textInfo)).append("\n MSL - " + main.getSeaLevel());
     }
 }
